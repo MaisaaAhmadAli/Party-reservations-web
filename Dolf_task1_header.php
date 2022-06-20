b6d99942731be59684a2e0269126e0645b4fcee7
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-06-20 12:32:44 +0300

    task1

---
+++ Dolf_task1_header.php
@@ -0,0 +1,15 @@
+    <html>
+    <head>
+        <title>Overview of the Electrical Power Systems</title>
+        <meta charset="utf-8" >
+         <link rel="stylesheet" type="text/css"  >
+         <link rel="stylesheet" href="styless.css">
+    </head>
+    <body>
+        <header>
+           <img src="HatchfulExport-All/logo.png" alt="M_logo" width="100" height="100" align="right">
+           <br> <br>
+           <h3 class="over"> Overview of the Electrical Power Systems</h3>
+           <hr class="up">
+        </header>
+    
---
+++ Dolf_task1_index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="styless.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/607de92be1.js" crossorigin="anonymous"></script>
+    <meta charset = "utf-8">
+    <style>
+    </style>
+<title> </title>
+   <?php require_once("Dolf_task1_header.php") ?>
+    </head>
+    <body>
+    <div class="menu">
+   <p> &nbsp;  &nbsp;  COURS MENU</p>
+        <hr>
+        <i class="fa-regular fa-clone" class="icones"></i><p>Components of Electrical Power System</p>
+        <i class="fa-regular fa-circle-play" class="icones"></i><a href="welcome_page.php">welcome !</a>
+  <a href="#"></a>
+  <a href="#"></a>
+  <a href="#"></a>
+  <p>Dummy Moudule 02 </p>
+</div>
+    </body>
+    </html>
+    
